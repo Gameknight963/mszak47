cf21e52ef2a9d96b2260d6a8628599f443d8cbcd
Gameknight963 <88798282+Gameknight963@users.noreply.github.com>
2026-04-24 23:54:57 -0500

    add more properties

--- Gun.cs
+++ Gun.cs
@@ -8,8 +8,17 @@
         public string? AudioFile { get; set; }
         public string? IconFile { get; set; }
         public string? HoleFile { get; set; }
+
         public float FireRate { get; set; } = 0.1f;
         public float AudioVolume { get; set; } = 0.5f;
+        public float Damage { get; set; } = 10f;
+        public float Range { get; set; } = 100f;
+
+        public float AdsSpeed { get; set; } = 0.2f;
+        public float AdsFov { get; set; } = 50f;
+        public float RecoilKickDuration { get; set; } = 0.05f;
+        public float RecoilRecoverDuration { get; set; } = 0.15f;
+        public float BulletHoleDuration { get; set; } = 10f;
 
         public float[] NormalPosition { get; set; } = [0f, 0f, 0f];
         public float[] AdsPosition { get; set; } = [0f, 0f, 0f];
