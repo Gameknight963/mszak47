4618ea4035b883fb9b6e21705b86d368361f6428
Gameknight963 <88798282+Gameknight963@users.noreply.github.com>
2026-04-24 22:50:02 -0500

    data driven

---
+++ Extensions.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace mszguns
+{
+    public static class Extensions
+    {
+        public static Vector3 ToVector3(this float[] arr) => new(arr[0], arr[1], arr[2]);
+    }
+}
